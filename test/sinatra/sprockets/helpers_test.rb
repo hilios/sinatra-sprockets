669231e55111da5218195550500e75c24dfd5a22
Edson Hilios <edson.hilios@telefonicabeta.com>
2013-05-28 11:48:04 -0300

    Add more tests for the helpers

--- test/sinatra/sprockets/helpers_test.rb
+++ test/sinatra/sprockets/helpers_test.rb
@@ -4,10 +4,36 @@ class Sinatra::Sprockets::HelpersTest < MiniTest::Test
 
   def app
     mock_app do
-      get('/javascript')  { javascript_tag('application') }
+      get('/asset/path')      { asset_path('image.gif') }
 
-      get('/stylesheet')  { stylesheet_tag('application') }
+      get('/javascript/tag')  { javascript_tag('application') }
+      get('/stylesheet/tag')  { stylesheet_tag('application') }
+
+      get('/javascript')      { javascript(:application) }
+      get('/javascripts')     { javascripts(:application, :other) }
+
+      get('/stylesheet')      { stylesheet(:application) }
+      get('/stylesheets')     { stylesheets(:application, :other) }
     end
+  end
+
+  def test_asset_path_helper
+    get('/asset/path')
+    assert_match %r(image-(.+?).gif),           last_response.body
+  end
+
+  def test_javascript_tag_helper
+    get('/javascript/tag')
+    assert last_response.ok?
+    assert_match %r(application-(.+?).js),      last_response.body
+    assert_match %r(<script (.+?)><\/script>),  last_response.body
+  end
+
+  def test_stylesheet_tag_helper
+    get('/stylesheet/tag')
+    assert last_response.ok?
+    assert_match %r(application-(.+?).css),     last_response.body
+    assert_match %r(<link (.+?)>),              last_response.body
   end
 
   def test_javascript_helper
@@ -15,13 +41,25 @@ class Sinatra::Sprockets::HelpersTest < MiniTest::Test
     assert last_response.ok?
     assert_match %r(application-(.+?).js),      last_response.body
     assert_match %r(<script (.+?)><\/script>),  last_response.body
+    # Pluralized
+    get('/javascripts')
+    assert last_response.ok?
+    assert_match %r(application-(.+?).js),      last_response.body
+    assert_match %r(other-(.+?).js),            last_response.body
+    assert_match %r(<script (.+?)></script>),   last_response.body
   end
 
   def test_stylesheet_helper
     get('/stylesheet')
     assert last_response.ok?
-    assert_match %r(application-(.+?).css), last_response.body
-    assert_match %r(<link (.+?)>),          last_response.body
+    assert_match %r(application-(.+?).css),     last_response.body
+    assert_match %r(<link (.+?)>),              last_response.body
+    # Pluralized
+    get('/stylesheets')
+    assert last_response.ok?
+    assert_match %r(application-(.+?).css),     last_response.body
+    assert_match %r(other-(.+?).css),           last_response.body
+    assert_match %r(<link (.+?)>),              last_response.body
   end
 
 end
